5cceeafd82867e333711b17c04e098eac2d185a1
Moosavi <moosavi238@gmail.com>
2022-08-01 17:54:51 +0430

    add return type in GetElement.py

--- GetElement.py
+++ GetElement.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class GetElement:
@@ -11,12 +12,12 @@ class GetElement:
         self.driver = webdriver.Firefox()
         self.driver.get(URL)
 
-    def wait_and_return(self, element, el_type=By.ID):
+    def wait_and_return(self, element, el_type=By.ID) -> WebElement:
         return WebDriverWait(self.driver, 60).until(
             EC.presence_of_element_located((el_type, element))
         )
 
-    def wait_and_returns(self, element, el_type=By.ID):
+    def wait_and_returns(self, element, el_type=By.ID) -> list[WebElement]:
         return WebDriverWait(self.driver, 60).until(
             EC.presence_of_all_elements_located((el_type, element))
         )
