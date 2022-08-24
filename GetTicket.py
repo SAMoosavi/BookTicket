92fa5d65bad25aa5f374fd6729e944a76e7093ea
Moosavi <moosavi238@gmail.com>
2022-08-24 10:32:11 +0430

    add __complete_form_search in GetTicket.py

--- GetTicket.py
+++ GetTicket.py
@@ -1,7 +1,9 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 from CompleteForms import CompleteForms
-from globalVariable import cities, months
+from globalVariable import cities, months, Sex
 
 
 class GetTicket(CompleteForms):
@@ -32,3 +34,91 @@ class GetTicket(CompleteForms):
 
     def __close_login_form(self):
         self.clickBtn("/html/body/div[2]/div/div/div[2]/div/span/span", By.XPATH, 1)
+
+    # search
+    def __complete_form_search(
+            self,
+            beginning: int, ending: int,
+            date: str,
+            adultNum: int | str, childNum: int | str,
+            sex: Sex
+    ):
+        self.__beginning(beginning)
+        self.__ending(ending)
+        self.__date(date)
+        self.__person_number(adultNum, childNum)
+        self.__sex(sex)
+
+    def __sex(self, data: Sex):
+        if data == Sex.WOMAN:
+            self.clickBtn(
+                "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[3]/div/div[2]/div[1]/div[4]/label[3]",
+                By.XPATH)
+        elif data == Sex.MAN:
+            self.clickBtn(
+                "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[3]/div/div[2]/div[1]/div[4]/label[2]",
+                By.XPATH)
+        elif data == Sex.BOTH:
+            self.clickBtn(
+                "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[3]/div/div[2]/div[1]/div[4]/label[1]",
+                By.XPATH)
+
+    def __person_number(self, adult: str | int, child: str | int):
+        self.clickBtn(
+            "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[3]/div/div[1]/div/label", By.XPATH)
+        for i in range(1, int(adult)):
+            self.clickBtn(
+                "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[3]/div/div[2]/div[1]/div[1]/div/button[2]",
+                By.XPATH)
+        for i in range(0, int(child)):
+            self.clickBtn(
+                "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/button[2]",
+                By.XPATH)
+
+    def __date(self, data: str):
+        self.__open_date()
+        self.__set_date(data)
+        self.__save_date()
+
+    def __save_date(self):
+        self.clickBtn(
+            "/html/body/div[1]/div/div/div[2]/div[2]/div[2]/form/div[2]/div[2]/div/div[2]/div[3]/div[2]/button[2]",
+            By.XPATH)
+
+    def __set_date(self, data: str):
+        date = data.split('/')
+        day = int(date[2])
+        index = "1" \
+            if self.__month[date[1]] in self.wait_and_return(
+            "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/div[1]/div[1]",
+            By.XPATH).text \
+            else "2"
+        btnsPath = "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/div[" \
+                   + index + \
+                   "]/div[3]/div[not(contains(@class, 'empty'))]"
+        self.click_btn_on_list(btnsPath, day - 1, By.XPATH)
+
+    def __open_date(self):
+        self.clickBtn("/html/body/div[1]/div/div/div[2]/div[2]/div[2]/form/div[2]/div[2]/div/div[1]/div[1]",
+                      By.XPATH)
+
+    def __ending(self, cityNumber: int):
+        inputPath = "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[2]/div/div/div[1]/div/label/input"
+        btnPath = "/html/body/div[1]/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[2]/div/div/div[2]/div/div/div[2]/div"
+        self.__set_city(cityNumber, inputPath, btnPath)
+
+    def __beginning(self, cityNumber: int):
+        inputPath = "/html/body/div/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[1]/div/div/div[1]/div/label/input"
+        btnPath = "/html/body/div[1]/div/div/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[1]/div/div/div[2]/div/div/div[2]/div"
+        self.__set_city(cityNumber, inputPath, btnPath)
+
+    def __set_city(
+            self, cityNumber: int,
+            inputPath: str, btnPath: str,
+            elTypeInput: By = By.XPATH, elTypeBtn: By = By.XPATH,
+            timeSleep: int = 1
+    ):
+        city = self.__cities[cityNumber]
+        self.complete_input_scroll(city, inputPath, elTypeInput)
+        time.sleep(timeSleep)
+        self.clickBtn(btnPath, elTypeBtn)
