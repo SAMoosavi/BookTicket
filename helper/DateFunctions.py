c3e0f27d2ffe365a81f254a8f625e1e79e3a2db8
moosavi <moosavi238@gmail.com>
2022-09-19 13:47:14 +0430

    edit DateFunctions.py j_to_g()

--- helper/DateFunctions.py
+++ helper/DateFunctions.py
@@ -3,7 +3,14 @@ import jdatetime
 
 def j_to_g(date: str, spliter='/') -> str:
     splitDate: list[str] = date.split(spliter)
-    return str(jdatetime.date(day=int(splitDate[2]), month=int(splitDate[1]), year=int(splitDate[0])).togregorian())
+    year = int(splitDate[0])
+    month = int(splitDate[1])
+    day = int(splitDate[2])
+
+    if year < 100:
+        year += 1300
+
+    return str(jdatetime.date(day=day, month=month, year=year).togregorian())
 
 
 def int_to_month_of_number(data: int) -> str:
