6ab83a7bbbc039295a2cc53e58195553af6957ee
moosavi <moosavi238@gmail.com>
2022-09-30 10:05:34 +0330

    bugfix in main

--- main.py
+++ main.py
@@ -33,13 +33,22 @@ passenger = Passenger(passenger_data['Email'], passenger_data['Mobile'], people,
 user = User(login_data['username'], login_data['password'], login_data['mobile'], login_data['email'])
 mr_bilit = MrBilitApiWrapper()
 
-while not mr_bilit.get_available(path_data['source'], path_data['destination'], path_data['date'], path_data['sex'],
-                                 list_ID):
-    time.sleep(3)
-mr_bilit.login(user.get_username(), user.get_password(), user.get_mobile())
-mr_bilit.reserve_seat()
-mr_bilit.register_info(passenger)
-mr_bilit.pay()
-while not mr_bilit.get_status():
-    time.sleep(3)
-mr_bilit.get_pdf()
+while True:
+    try:
+        i = 0
+        while not mr_bilit.get_available(path_data['source'], path_data['destination'], path_data['date'], path_data['sex'],
+                                         list_ID):
+            i += 1
+            print(i, " not found please wait!")
+            time.sleep(20)
+        print("found")
+        mr_bilit.login(user.get_username(), user.get_password(), user.get_mobile())
+        mr_bilit.reserve_seat()
+        mr_bilit.register_info(passenger)
+        mr_bilit.pay()
+        while not mr_bilit.get_status():
+            time.sleep(3)
+        mr_bilit.get_pdf()
+        break
+    except:
+        time.sleep(20)
