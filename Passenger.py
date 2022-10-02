bb1a6a27c6f5e1a2187eb2093906721bfd56b756
moosavi <moosavi238@gmail.com>
2022-10-02 10:06:54 +0330

    edit Passenger.py

--- Passenger.py
+++ Passenger.py
@@ -1,55 +1,36 @@
+from dataclasses import dataclass
+
 from helper.DateFunctions import jalali_to_gregorian
-from helper.validation import validation_email, validation_mobile, required, validation_national_code
 
 
-def validation_person(person: dict) -> bool:
-    return \
-        required(person['PersianFirstName']) and \
-        required(person['PersianLastName']) and \
-        validation_national_code(person['NationalCode']) and \
-        required(person['BirthDay']) and \
-        'TrainCars' in person and \
-        'TrainCapacityOptionalService' in person and \
-        'Male' in person
-
-
-def validation_people(people: list[dict]) -> bool:
-    for person in people:
-        if not validation_person(person):
-            return False
-    return True
-
-
-def set_birth_day_people(people: list[dict]):
-    for person in people:
-        person['BirthDay'] = jalali_to_gregorian(person['BirthDay'])
-
-
+@dataclass
 class Passenger:
-    __email: str
-    __mobile: str
-    __people: list[dict]
-    __phone: str
-
-    def __init__(self, email: str, mobile: str, people: list[dict], phone: str):
-        if validation_email(email) and validation_people(people):
-            mobile = validation_mobile(mobile)
-            if len(phone):
-                phone = validation_mobile(phone)
-            set_birth_day_people(people)
-
-            self.__email = email
-            self.__mobile = mobile
-            self.__people = people
-            self.__phone = phone
-        else:
-            raise "not correct data"
+    email: str
+    mobile: str
+    phone: str
+    persian_first_name: str
+    persian_last_name: str
+    male: bool
+    birth_day: str
+    national_code: str
+    train_cars: list
+    train_capacity_optional_service: dict
 
     def get_dict(self, bill_ID) -> dict:
         return {
-            "Email": self.__email,
-            "Mobile": self.__mobile,
-            "People": self.__people,
-            "Phone": self.__phone,
+            "Email": self.email,
+            "Mobile": self.mobile,
+            "People": [
+                {
+                    "PersianFirstName": self.persian_first_name,
+                    "PersianLastName": self.persian_last_name,
+                    "Male": self.male,
+                    "BirthDay": jalali_to_gregorian(self.birth_day),
+                    "NationalCode": self.national_code,
+                    "TrainCars": self.train_cars,
+                    "TrainCapacityOptionalService": self.train_capacity_optional_service
+                }
+            ],
+            "Phone": self.phone,
             "BillID": bill_ID
         }
