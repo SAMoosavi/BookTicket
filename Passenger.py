04a8f7989728f47ee2e961706cffc8515ecd6e68
SAMoosavi <90914468+SAMoosavi@users.noreply.github.com>
2022-10-09 16:10:14 +0330

    Change location get dict from passenger to mr bilit api wrapper (#8)
    * delete get_dict from Passenger.py
    
    * add __get_dict from Passenger.py

--- Passenger.py
+++ Passenger.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass
 
-from helper.DateFunctions import jalali_to_gregorian
-
 
 @dataclass
 class Passenger:
@@ -15,22 +13,3 @@ class Passenger:
     national_code: str
     train_cars: list
     train_capacity_optional_service: dict
-
-    def get_dict(self, bill_ID) -> dict:
-        return {
-            "Email": self.email,
-            "Mobile": self.mobile,
-            "People": [
-                {
-                    "PersianFirstName": self.persian_first_name,
-                    "PersianLastName": self.persian_last_name,
-                    "Male": self.male,
-                    "BirthDay": jalali_to_gregorian(self.birth_day),
-                    "NationalCode": self.national_code,
-                    "TrainCars": self.train_cars,
-                    "TrainCapacityOptionalService": self.train_capacity_optional_service
-                }
-            ],
-            "Phone": self.phone,
-            "BillID": bill_ID
-        }
