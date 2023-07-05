1b4ae92e0252795975e2d800eaeb20ea0e4acb67
moosavi <moosavi238@gmail.com>
2023-07-05 10:54:15 +0330

    creat promis

---
+++ book-ticket/src/functions/promis.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export function useSleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function useApi(
+	url: string,
+	config: AxiosRequestConfig,
+	accept?: (response: AxiosResponse) => void,
+	error?: (err: any) => void
+): Promise<AxiosResponse> {
+	return new Promise(async (resolve, reject) => {
+		await axios
+			.get(url, config)
+			.then((response) => {
+				if (accept) accept(response);
+				resolve(response);
+			})
+			.catch((err) => {
+				if (error) error(err);
+				reject(err);
+			});
+	});
+}
