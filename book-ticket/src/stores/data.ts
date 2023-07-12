e43609d915d6aa09b6da13478d4a20ab3304a0f6
moosavi <moosavi238@gmail.com>
2023-07-12 09:54:07 +0330

    add train

--- book-ticket/src/stores/data.ts
+++ book-ticket/src/stores/data.ts
@@ -1,10 +1,22 @@
 import { defineStore } from 'pinia';
 import { reactive, ref } from 'vue';
+import type { GetAvailableParameters } from '../functions/MrBilitApiWrapper.d';
 
 export const useData = defineStore('data', () => {
 	const data = reactive<{ mobile: string; email: string }>({
 		mobile: '',
 		email: '',
+	});
+
+	const train = reactive<GetAvailableParameters>({
+		from: 0,
+		to: 0,
+		date: '',
+		adultCount: 1,
+		childCount: 0,
+		infantCount: 0,
+		exclusive: false,
+		availableStatus: 'Both',
 	});
 
 	const reserve = ref<number[]>([]);
@@ -29,5 +41,23 @@ export const useData = defineStore('data', () => {
 		return reserve.value;
 	}
 
-	return { setMobile, setEmail, setReserve, getReserve, getData };
+	function setTrain(Train: GetAvailableParameters) {
+		train.from = Train.from;
+		train.to = Train.to;
+		train.date = Train.date;
+	}
+
+	function getTrain(): GetAvailableParameters {
+		return train;
+	}
+
+	return {
+		setMobile,
+		setEmail,
+		setReserve,
+		getReserve,
+		getData,
+		setTrain,
+		getTrain,
+	};
 });
