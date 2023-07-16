2f27ee3770dd85390c9c78197ed4bc97c89bd7ff
moosavi <moosavi238@gmail.com>
2023-07-16 08:23:42 +0330

    complete all api

--- book-ticket/src/functions/MrBilitApiWrapper.ts
+++ book-ticket/src/functions/MrBilitApiWrapper.ts
@@ -1,19 +1,98 @@
-import { useApi } from './promis';
+import { useApiGet, useApiPost } from './promis';
 import { useJalaliToGregorian } from './date';
 import type {
 	GetAvailableParameters,
 	LoginParameters,
+	ReserveSeatParameters,
 } from './MrBilitApiWrapper.d';
+import { useToken } from 'stores/token';
+import { useUser } from 'stores/user';
+import { useData } from 'stores/data';
+
+const token = useToken();
 
 export async function useLogin(params: LoginParameters) {
-	return await useApi('https://auth.mrbilit.com/api/login', { params });
+	return await useApiGet('https://auth.mrbilit.com/api/login', { params });
 }
 
 export async function useGetAvailable(params: GetAvailableParameters) {
-	return await useApi(
+	return await useApiGet(
 		'https://train.atighgasht.com/TrainService/api/GetAvailable/v2',
 		{
 			params: { ...params, date: useJalaliToGregorian(params.date) },
+		}
+	);
+}
+
+export async function useReserveSeat(params: ReserveSeatParameters) {
+	return await useApiGet(
+		'https://train.atighgasht.com/TrainService/api/ReserveSeat',
+		{
+			params: {
+				trainID: params.trainID,
+				adultCount: '1',
+				childCount: '0',
+				infantCount: '0',
+				includeOptionalServices: true,
+				exclusive: false,
+				genderCode: params.genderCode,
+				seatCount: '1',
+			},
+			headers: token.getHeader(),
+		}
+	);
+}
+
+export async function useRegisterInfo(bill_ID: number) {
+	const User = useUser();
+	const user = User.getUser();
+
+	const Data = useData();
+	const data = Data.getData();
+
+	return await useApiPost(
+		'https://train.atighgasht.com/TrainService/api/RegisterInfo',
+		{
+			Email: data.email,
+			Mobile: data.mobile,
+			People: [
+				{
+					PersianFirstName: user.persianFirstName,
+					PersianLastName: user.persianLastName,
+					Male: user.male,
+					BirthDay: useJalaliToGregorian(user.birthDay),
+					NationalCode: user.nationalCode,
+					TrainCars: [],
+					TrainCapacityOptionalService: {},
+				},
+			],
+			Phone: '',
+			BillID: bill_ID,
+		},
+		{
+			headers: token.getHeader(),
+		}
+	);
+}
+
+export async function usePay(bill_code: string) {
+	return await useApiGet(
+		`https://payment.mrbilit.com/api/billpayment/${bill_code}`,
+		{
+			params: {
+				payFromCredit: true,
+				access_token: token.getToken(),
+			},
+			headers: token.getHeader(),
+		}
+	);
+}
+
+export async function useGetStatus(bill_code: number, mac: string) {
+	return await useApiGet(
+		`https://finalize.mrbilit.com/api/workflow/bill/${bill_code}/status`,
+		{
+			params: { mac: mac },
 		}
 	);
 }
--- book-ticket/src/functions/promis.ts
+++ book-ticket/src/functions/promis.ts
@@ -5,7 +5,7 @@ export function useSleep(ms: number) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export function useApi(
+export function useApiGet(
 	url: string,
 	config: AxiosRequestConfig,
 	accept?: (response: AxiosResponse) => void,
@@ -14,6 +14,27 @@ export function useApi(
 	return new Promise(async (resolve, reject) => {
 		await axios
 			.get(url, config)
+			.then((response) => {
+				if (accept) accept(response);
+				resolve(response);
+			})
+			.catch((err) => {
+				if (error) error(err);
+				reject(err);
+			});
+	});
+}
+
+export function useApiPost(
+	url: string,
+	data: any,
+	config: AxiosRequestConfig,
+	accept?: (response: AxiosResponse) => void,
+	error?: (err: any) => void
+): Promise<AxiosResponse> {
+	return new Promise(async (resolve, reject) => {
+		await axios
+			.post(url, data, config)
 			.then((response) => {
 				if (accept) accept(response);
 				resolve(response);
