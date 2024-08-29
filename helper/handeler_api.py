f9cfc36be4c2f696c42e2560472e8fa5f07a7d9a
SAMoosavi <moosavi238@gmail.com>
2024-08-29 14:52:55 +0330

    fix urls

--- helper/handeler_api.py
+++ helper/handeler_api.py
@@ -6,9 +6,13 @@ def api_handler(url, params=None, headers=None, json=None, method="get", error_m
     while True:
         try:
             response = request(method, url, params=params, headers=headers, json=json)
-            return response
+            # print("url ", response.url)
+            if 200 <= response.status_code < 300:
+                return response
+            else:
+                raise RequestException(response.text)
         except RequestException as e:
             print(f"Error in API handler: {e}")
             if error_msg:
                 print(error_msg)
-            sleep(20)
+            sleep(60)
